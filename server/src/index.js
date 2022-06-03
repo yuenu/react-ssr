252b2ee5a9e5b8f0080fe2a397b23dbd1ffeb3df
yuenu <miraiku82@gmail.com>
2022-06-03 15:10:37 +0800

    feat: server configuration

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,22 @@ import Home from "./client/components/Home";
 
 const app = express();
 
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   const content = ReactDOMServer.renderToString(<Home />);
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div>${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
   res.send(content);
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000, http://localhost:3000");
 });
