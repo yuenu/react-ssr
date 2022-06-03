b16a194d5dc61d1896033192b5565df2c32fc0ff
yuenu <yuenuni@hotmail.com>
2022-06-03 22:39:42 +0800

    feat: refactoring for clraner code

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,9 @@
 const path = require("path");
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
-module.exports = {
+const config = {
   // Inform webpack that we're building a bundle
   // For nodeJS, rather than for the browser
   target: "node",
@@ -16,21 +19,7 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
   },
 
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { target: { browsers: ["last 2 versions"] } }],
-          ],
-        },
-      },
-    ],
-  },
+  externals: [webpackNodeExternals()],
 };
+
+module.exports = merge(baseConfig, config);
