7617498f09a865e456e353caab5990b60bad4c9d
yuenu <miraiku82@gmail.com>
2022-06-15 22:55:17 +0800

    feat: server side data loading

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import Home from "./components/Home";
-import UserList from "./components/UserList";
+import UserList, { loadData } from "./components/UserList";
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/users" component={UserList}></Route>
-    </div>
-  );
-};
+export default [
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+  },
+  {
+    loadData,
+    path: "/users",
+    component: UserList,
+  },
+];
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import "babel-polyfill";
 import express from "express";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
@@ -9,10 +11,13 @@ app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
 
-  // Some logic to initialize
-  // and load data into the store
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
 
-  res.send(renderer(req, store));
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
