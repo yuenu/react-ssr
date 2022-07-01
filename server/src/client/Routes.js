1b1579a53c2379a040b0f8934b09e8d4ca5e2f2c
yuenu <miraiku82@gmail.com>
2022-07-01 23:00:04 +0800

    feat: authentication in a server side rendering world

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,15 +1,21 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
 import UserListPage from "./pages/UserListPage";
+import App from "./App";
 
 export default [
   {
-    ...HomePage,
-    path: "/",
-    exact: true,
-  },
-  {
-    ...UserListPage,
-    path: "/users",
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: "/",
+        exact: true,
+      },
+      {
+        ...UserListPage,
+        path: "/users",
+      },
+    ],
   },
 ];
--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div>I'm the home component</div>
-      <button onClick={() => console.log("clicked")}>presse me!</button>
+    <div className="center-align" style={{ marginTop: "200px" }}>
+      <h3>Weclome</h3>
+      <p>Check out these awesome features</p>
     </div>
   );
 };
